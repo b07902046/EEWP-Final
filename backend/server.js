02313b3d6f2a8a397f7655637e825a1ea9b1e6e4
obe1 <b07902046@ntu.edu.tw>
2021-01-15 22:35:49 +0800

    Solve import dotenv issue

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,38 @@
-import express from 'express';
-const app = express();
-const port = process.env.PORT || 4000
-app.get('/', (req, res) => {
-		res.send("Hello World!");
+import dotenv from 'dotenv'
+import GraphQLServer from 'graphql-yoga'
+import PubSub from 'graphql-yoga'
+import mongoose from 'mongoose'
+
+dotenv.config()
+if (!process.env.MONGO_URL) {
+	console.error('Missing MONGO_URL!!!')
+	process.exit(1)
+}
+
+mongoose.connect(process.env.MONGO_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+})
+
+const db = mongoose.connection
+
+db.on('error', (error) => {
+	console.error(error)
+})
+
+db.once('open', () => {
+	console.log('MongoDB connected!')
+})
+
+const pubsub = new PubSub()
+
+const server = new GraphQLServer({
+  typeDefs: './server/schema.graphql',
+  resolvers: {
+  },
+  context: {
+    db,
+    pubsub
+  }
 });
 
-app.listen(port, () => {
-		console.log(`listen on port ${port}`);
-});
